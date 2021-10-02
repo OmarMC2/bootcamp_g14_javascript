56da9f3050596f14c959cc1598ddb94ed50d4934
Omar <OmarMC2@github.com>
2021-10-01 22:47:09 -0500

    afinados los últimos detalles de los ejercicios en clase, perdón por la demora DX

--- clase-2/js/scripts.js
+++ clase-2/js/scripts.js
@@ -1,49 +1,66 @@
 /*
-let number = prompt(Number)
-if (number % 2 == 0) {
-    console.log('Su numero es par')
+let number = Number(prompt('por favor ingrese un numero entre el 1 y el 100'));
+if (number % 2 == 0 && number >= 1 && number <= 100) {
+    console.log('Su número es par');
+} else if (number % 2 != 0 && number >= 1 && number <= 100) {
+    console.log('su número es impar');
 } else {
-    console.log('su numero es impar')
+    console.log('Error, inténtelo de nuevo con un valor o caracter válido');
 }
+
+
 */
 
 /*
 let x, y
-x = prompt(Number);
-y= prompt(Number);
+x = Number(prompt('Por favor digite un número'));
+y= Number(prompt('Por favor digite otro número'));
 
 if (x > y) {
-    console.log("el primer numero es mayor y el segundo es menor")
+    console.log("El número", x,  "es mayor que" , y);
+} else if (x < y) {
+    console.log("Al número", y,  "es mayor que" , x);
+} else if (x == y) {
+    console.log("Ambos números son iguales");
 } else {
-    console.log("el primer numero es menor y el segundo es mayor")
+    console.log ('Error, inténtelo de nuevo con un caracter válido');
+}
+
+*/
+
+/*
+
+let nombre, pesoT, pesoL ;
+nombre = String (prompt('Por favor teclé su nombre'));
+pesoT = Number (prompt("Por favor digite su peso en kg"));
+pesoL = pesoT * .165392066891 ;
+if (pesoT >=0) {
+    console.log(nombre, 'su peso en la luna es de:', pesoL, "kg");
+} else {
+    console.log ('Error, inténtelo de nuevo con un caracter válido');
 }
 */
 /*
-let nombre, pesoT, pesoL
-nombre = prompt('')
-pesoT = prompt(Number)
-pesoL = pesoT * .165392066891 ;
 
-console.log('su peso en la luna es de:', pesoL);
-*/
 
-/*
-let cal = prompt(Number) ;
+let cal = Number(prompt("Por favor digite una calificación que se encuentre entre los números 1 y 100")) ;
 
-if (cal >= 90) {
-    console.log('Su calilificacion es de A')
+if (cal >= 90 &&  cal <= 100) {
+    console.log('Su calilificacion es de A');
 } else if (cal >= 80 && cal < 90){
-    console.log('Su calilificacion es de B')
+    console.log('Su calilificacion es de B');
     
 } else if (cal >= 70 && cal < 80) {
-    console.log('Su calilificacion es de C')
+    console.log('Su calilificacion es de C');
 } else if (cal >= 60 && cal < 70) {
-    console.log('Su calilificacion es de D')
+    console.log('Su calilificacion es de D');
 } else if (cal >= 50 && cal < 60) {
-    console.log('Su calilificacion es de E')
-} else if (cal < 50) {
-    console.log('Su calilificacion es de F')
+    console.log('Su calilificacion es de E');
+} else if (cal < 50 && cal >= 0) {
+    console.log('Su calilificacion es de F');
 } else {
-    console.log('Error')
+    console.log ('Error, inténtelo de nuevo con un caracter válido');
+
 }
+
 */
