d5d982cad9e528da8d0a98a00f77be4eda677a96
Omar <OmarMC2@github.com>
2021-10-01 21:57:45 -0500

    añado mis ejercicios de la clase a la carpeta clase-2

---
+++ clase-2/js/scripts.js
@@ -0,0 +1,49 @@
+/*
+let number = prompt(Number)
+if (number % 2 == 0) {
+    console.log('Su numero es par')
+} else {
+    console.log('su numero es impar')
+}
+*/
+
+/*
+let x, y
+x = prompt(Number);
+y= prompt(Number);
+
+if (x > y) {
+    console.log("el primer numero es mayor y el segundo es menor")
+} else {
+    console.log("el primer numero es menor y el segundo es mayor")
+}
+*/
+/*
+let nombre, pesoT, pesoL
+nombre = prompt('')
+pesoT = prompt(Number)
+pesoL = pesoT * .165392066891 ;
+
+console.log('su peso en la luna es de:', pesoL);
+*/
+
+/*
+let cal = prompt(Number) ;
+
+if (cal >= 90) {
+    console.log('Su calilificacion es de A')
+} else if (cal >= 80 && cal < 90){
+    console.log('Su calilificacion es de B')
+    
+} else if (cal >= 70 && cal < 80) {
+    console.log('Su calilificacion es de C')
+} else if (cal >= 60 && cal < 70) {
+    console.log('Su calilificacion es de D')
+} else if (cal >= 50 && cal < 60) {
+    console.log('Su calilificacion es de E')
+} else if (cal < 50) {
+    console.log('Su calilificacion es de F')
+} else {
+    console.log('Error')
+}
+*/
